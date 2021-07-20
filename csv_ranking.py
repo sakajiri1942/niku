b01c540f46b8e58c8745fe29941d3c70c9540848
sakajiri1942 <sakajiri@sokki-ltd.co.jp>
2021-07-20 17:33:19 +0900

    会社のPC0715-17:20

--- csv_ranking.py
+++ csv_ranking.py
@@ -5,83 +5,45 @@ import urllib.request, urllib.error
 from bs4 import BeautifulSoup
 from numpy import array
 
+#出力ファイル
+out_name = ('C:/xampp/htdocs/niku/ブランド牛のランキング.txt')
+
 # fname= ('C:/Users/sakajiri/PycharmProjects/niku/nikuniku-2021-07-15.csv')
-fname = ('C:/Users/sekis/PycharmProjects/niku/nikuniku-2021-07-15.csv')
-f = open(fname, 'r', encoding="cp932")
+fname = ('C:/xampp/htdocs/niku/ブランド牛の各ページ情報-追加中（重要）.txt')
+f = open(fname, 'r',encoding='utf-8')
 data = f.readlines()
 f.close()
 
 a = 0
+brand_count_all = '';
 for l_data in data:
-    # time.sleep(1)
+    time.sleep(1)
     # l_data = l_data.replace('\n','')
     a = a + 1
-    if '全国' not in l_data:
-        # print(a, l_data)
-        kenbetu_usi = []
-        kenbetu_usi = l_data.split(',')
+    kenbetu_usi = []
+    kenbetu_usi = l_data.split(',')
+    #print(kenbetu_usi[2])
+    s_quote = urllib.parse.quote(kenbetu_usi[2])
+    url = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?applicationId=1093881737175375236&keyword=' + s_quote + '&genreId=110428&format=xml'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    try:
+        # aaaa = 0
+        print(kenbetu_usi[2], soup.count.text)
+        brand_count_all = brand_count_all + kenbetu_usi[0]+','+ kenbetu_usi[2] + ','+ kenbetu_usi[1]+','+ kenbetu_usi[3] + ','+str(soup.count.text)+"\n"
+    except:
+        print('Error')
 
-        # 県名をはじく 県名は"kenbetu_usi[0]"
-        for l_kenbetu_usi in kenbetu_usi:
 
-            if '(' not in l_kenbetu_usi and len(l_kenbetu_usi) > 2:  # 半角（）
-                time.sleep(1)
-                l_kenbetu_usi = l_kenbetu_usi.replace('\n', '')
-                # print (l_kenbetu_usi)
-
-                s_quote = urllib.parse.quote(l_kenbetu_usi)
-                url = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?applicationId=1093881737175375236&keyword=' + s_quote + '&genreId=110428&format=xml'
-                # url = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?applicationId=1093881737175375236&keyword=%E7%A6%8F%E8%A2%8B&genreId=110428'
-
-                # bs4以外のURL情報取得　あとで削除
-                # req = urllib.request.Request(url)
-                # with urllib.request.urlopen(req) as res:
-                #    body = res.read().decode("utf-8")
-                # print(body)
-
-                r = requests.get(url)
-                soup = BeautifulSoup(r.text, 'html.parser')
-                try:
-                    # aaaa = 0
-                    print(l_kenbetu_usi, soup.count.text)
-
-                except:
-                    print('Error')
-
-                # プライスの処理
-                # print (soup.find_all('itemPrice'))
-                price_moto = soup.find_all('itemprice')
-                # print(price_moto)
-                p = 0  # price_saki = []
-                price_all = ''
-                for l_price_moto in price_moto:
-                    l_price_moto = str(l_price_moto)
-                    l_price_moto = l_price_moto.replace('<itemprice>', '')
-                    l_price_moto = l_price_moto.replace('</itemprice>', '')
-                    # print(l_price_moto)
-                    # l_price_moto = l_price_moto.replace('\'', '')
-                    price_all = price_all + l_price_moto + ','
-
-                # print(price_all)
-                price_set = []
-                price_set = price_all.split(',')
-                price_set.remove('')
-                price_set_int = list(map(int, price_set))
-
-                price_set_int_sort = sorted(price_set_int)
-                # print(price_set_int_sort)
-                moto_kazu = len(price_set_int_sort)
-
-                if moto_kazu > 0:
-                    matome_en = str(price_set_int_sort[0]) + '～' + str(price_set_int_sort[-1]) + '円'
-                    print(matome_en)
-
-                # array(aaa)
-                # bbb = aaa.replace('<itemprice>','')
-                # bbb = bbb.replace('</itemprice>', '')
-                # ccc = aaa.split(',')
-                # print(aaa)
-
-    if a == 2:
+    if '終わり' in l_data:
         break
+    #if a == 2:
+    #   break
+
+
+f = open(out_name, 'w', encoding="utf-8")
+f.writelines(brand_count_all)
+f.close()
+
+
 print("ok")
---
+++ html_to_地図ページ作成.py
@@ -0,0 +1,104 @@
+# SIRIUS が作成したファイルをPHPの実行フォルダに作成する。
+# 日本地図バージョン
+
+
+##########
+# ファイル系
+# 元ファイル
+moto_f = 'C:/ACES WEB/SIRIUS/サイト生成先/肉のサイト/brand_beef/index.php'
+# 出力ファイル
+saki_f = 'C:/xampp/htdocs/niku/niku-site/brand_beef/index.php'
+# 必要ファイル
+burand_f = 'C:/xampp/htdocs/niku/ブランド牛の各ページ情報-追加中（重要）.txt'
+#kenmei_f = 'C:/xampp/htdocs/niku/html_to_県名.txt'
+
+f = open(moto_f, 'r', encoding="utf-8")
+m_data = f.readlines()
+f.close()
+
+f = open(burand_f, 'r', encoding="utf-8")
+b_data = f.readlines()
+f.close()
+
+#県名だけを抜き取ったファイルの作成　カウント用
+ken_serch_all = ''
+for l_b_data in b_data:
+    ken_serch = l_b_data.split(',')
+    ken_serch_all = ken_serch_all+ken_serch[0]
+
+#print (ken_serch_all)
+
+a = 0
+for l_b_data in b_data:
+    # print (l_b_data)
+    ken1 = l_b_data.split(',')
+    ken2 = b_data[a+1].split(',')
+
+    okikae = '<p>' + ken1[0] + '</p>'
+    okikae = okikae.replace('\n', '')
+    okikae = okikae.replace('県', '')
+    okikae = okikae.replace('東京都', '東京')
+    okikae = okikae.replace('府', '')
+    # 全国の数を変数にいれておく
+    zenkoku_kazu = len(b_data) - 1
+    #print(okikae)
+
+
+
+
+    #県名カウント
+    #次の行の県名といまの行の県名が違ったら実行
+    if ken2[0] not in ken1[0]:
+        #print (ken1[0],ken2[0])
+        ken_kazu = ken_serch_all.count(ken1[0])
+        #print (ken_kazu)
+        kenmei_roma = ken1[1]
+        kenmei_roma = kenmei_roma.replace('\n','')
+        #元ファイルの置き換え
+        b = 0
+        for l_moto_f in m_data:
+
+            if okikae in l_moto_f:
+
+
+                okikae_ato = '<p>' + ken1[0] +'('+str(ken_kazu)+ ')</p>'
+                m_data[b] = m_data[b].replace(okikae, okikae_ato)
+
+                okikae_ken = './brand_beef_' + kenmei_roma + '.php'
+                if (b - 2) > 0:
+
+                    #print(kenmei_roma)
+                    m_data[b - 2] = m_data[b - 2].replace('#', okikae_ken)
+                    #print(m_data[b-2])
+                #print(m_data[b])
+            b = b + 1
+
+
+
+    if '終わり' in b_data[a+1]:
+        c = 0
+        for l_moto_f in m_data:
+
+            if '現在〇種' in m_data[c]:
+                okikae_zentou = '現在'+str(zenkoku_kazu)+'種'
+                m_data[c] = m_data[c].replace('現在〇種',okikae_zentou)
+                print(m_data[c])
+            c = c + 1
+        break
+
+
+
+
+
+
+    a = a + 1
+
+    #print(len(b_data))
+
+    # break
+
+f = open(saki_f, 'w', encoding="utf-8")
+f.writelines(m_data)
+f.close()
+
+print("ok")
