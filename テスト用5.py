a7f0750bc9e6d3da3b8d35ae38cf78a85f07970c
sakajiri1942 <sakajiri@sokki-ltd.co.jp>
2021-08-06 11:36:19 +0900

    806

--- テスト用5.py
+++ テスト用5.py
@@ -1,6 +1,25 @@
-import numpy as np
-import matplotlib.pyplot as plt
-left = np.array([1, 2, 3, 4, 5])
-height = np.array([100, 200, 300, 400, 500])
-plt.bar(left, height)
-plt.show()
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+mozi = datasets.load_digits()
+
+
+X = mozi.data
+y = mozi.target
+
+(X_train, X_test, y_train, y_test) = train_test_split(X, y, )
+
+from sklearn import svm
+clf = svm.SVC(gamma=0.001)
+
+clf.fit(X_train, y_train)
+
+from sklearn import metrics
+
+accuracy = clf.score(X_test, y_test)
+print(f"正解率{accuracy}")
+
+predicted = clf.predict(X_test)
+
+
+print(metrics.classification_report(y_test, predicted))
--- テスト用6.py
+++ テスト用6.py
@@ -1,5 +1,11 @@
-aaa = [1,3,4,9,10,88]
-aaa.pop(0)
-print(aaa)
-aaa.pop()
-print(aaa)
+aaa = '名前,年齢,性別,職業\n\
+坂田,22,男,会社員\n\
+宮森,31,女,家事手伝\n\
+横田,14,女,学生\n\
+山岡,52,男,自営業'
+bbb = aaa.split('\n')
+print(bbb)
+for ccc in bbb:
+    if '女' in ccc:
+        ddd = ccc.split(',')
+        print(ddd[0])
